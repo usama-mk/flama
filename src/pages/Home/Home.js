ef56dc4f3c7df1b47dca8c00612e1b01caccaeaf
Usama Muhammad <usamamuhammad45678@gmail.com>
2021-09-03 18:11:48 +0500

    background added with divs in place

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="homeLeft">
+        <div id="Flama" class="Flama">
+          <div className="flama_m" id="Flama_m">
+            <span>Flama</span>
+          </div>
+        </div>
+
+        <div>icons</div>
+      </div>
+
+      <div className="homeRight">
+        <div>bio</div>
+        <div>CAaa</div>
+        <div>musica</div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
