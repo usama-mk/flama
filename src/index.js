e83c2ed2d0e63b5f0621e795390e8643b2b22cb7
Usama Muhammad <usamamuhammad45678@gmail.com>
2021-09-04 10:55:51 +0500

    modal fixed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Modal from 'react-modal';
 
+Modal.setAppElement('#root');
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
+import Modal from 'react-modal';
+import { ProGallery } from 'pro-gallery';
+import 'pro-gallery/dist/statics/main.css';
 
+const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      // backgroundColor: 'green',
+      // opacity: '1'
+    boxShadow: 'rgba(0, 0, 0, 0.96) 0px 30px 70px 70px',
+
+      
+    },
+    
+    
+    overlay: {
+      // opacity:' 0.55',
+      // filter: 'blur(20px)',
+      // fill: 'rgba(0,0,0,1)',
+      // background: 'black'
+      backgroundColor: 'none'
+    }
+  };
+
+ 
 function Home() {
+    let subtitle;
+    const [modalIsOpen, setModalIsOpen]= useState(true)
+
+    function afterOpenModal() {
+         
+        subtitle.style.color = '#f00';
+      }
   return (
-    <div className="home">
+    <div style={{backgroundColor: modalIsOpen? 'black' :'', opacity: modalIsOpen? '0.55' :''}} className="home">
       <div className="homeLeft">
         <div id="Flama" class="Flama">
           <div className="flama_m" id="Flama_m">
@@ -15,10 +51,28 @@ function Home() {
       </div>
 
       <div className="homeRight">
-        <div>bio</div>
-        <div>CAaa</div>
-        <div>musica</div>
+        <div className="bio" onClick={()=>setModalIsOpen(true)}>bio</div>
+        <div className="bio"  >Gallery</div>
+        <div className="bio">musica</div>
       </div>
+    
+       
+       <Modal
+         isOpen={modalIsOpen}
+         onAfterOpen={afterOpenModal}
+         onRequestClose={()=>setModalIsOpen(false)}
+         style={customStyles}
+         contentLabel="Example Modal"
+         
+       >
+        
+         <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+         <button onClick={()=>setModalIsOpen(false)}>close</button>
+         <div>I am a modal</div>
+       
+         
+       </Modal>
+     
     </div>
   );
 }
