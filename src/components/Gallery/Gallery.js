88f50d1e5794cd3d4aa231e5b836d696ee7fba2b
Usama Muhammad <usamamuhammad45678@gmail.com>
2021-09-04 11:55:31 +0500

    gallery modal skeleton done

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import './Gallery.css'
+import g1 from '../../assets/g1.png'
+import g2 from '../../assets/g2.png'
+import g3 from '../../assets/g3.png'
 
-function Gallery() {
+function Gallery({setModalIsOpen}) {
+    const images=[
+        {
+            src: g1,
+            width: '200px',
+            height: '300px'
+        },
+        {
+            src: g2,
+            width: '150px',
+            height: '200px'
+        },
+        {
+            src: g3,
+            width: '200px',
+            height: '150px'
+        },
+        {
+            src: g1,
+            width: '200px',
+            height: '300px'
+        },{
+            src: g1,
+            width: '200px',
+            height: '300px'
+        },
+    ]
     return (
         <div className="gallery">
-            
+            {
+                images.map((image)=>{
+                    return(
+                        <div className="galleryItem" >
+                            <img src={image.src} height={image.height} width={image.width} alt="" />
+                        </div>
+                    )
+                })
+            }
+
+        {/* <button onClick={()=>setModalIsOpen(false)}>close</button> */}
         </div>
     )
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,47 +1,44 @@
 import React, { useState } from "react";
 import "./Home.css";
-import Modal from 'react-modal';
-import { ProGallery } from 'pro-gallery';
-import 'pro-gallery/dist/statics/main.css';
+import Modal from "react-modal";
+import { ProGallery } from "pro-gallery";
+import "pro-gallery/dist/statics/main.css";
 import Gallery from "../../components/Gallery/Gallery";
 
 const customStyles = {
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-      // backgroundColor: 'green',
-      // opacity: '1'
-    boxShadow: 'rgba(0, 0, 0, 0.96) 0px 30px 70px 70px',
-      backgroundColor: 'transparent',
-      border: 'none',
-    },
-    
-    backgroundColor: 'transparent',
-    
-    overlay: {
-      // opacity:' 0.55',
-      // filter: 'blur(20px)',
-      // fill: 'rgba(0,0,0,1)',
-      // background: 'black'
-      backgroundColor: 'none'
-    }
-  };
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    // backgroundColor: 'green',
+    boxShadow: "rgba(0, 0, 0, 0.96) 0px 30px 70px 70px",
+    border: "none",
+    backgroundColor: 'rgba(0, 0, 0, 0.55)',
+  },
 
- 
+  overlay: {
+    backgroundColor: "none",
+  },
+};
+
 function Home() {
-    let subtitle;
-    const [modalIsOpen, setModalIsOpen]= useState(true)
+  let subtitle;
+  const [modalIsOpen, setModalIsOpen] = useState(true);
 
-    function afterOpenModal() {
-         
-        subtitle.style.color = '#f00';
-      }
+  function afterOpenModal() {
+    subtitle.style.color = "#f00";
+  }
   return (
-    <div style={{backgroundColor: modalIsOpen? 'black' :'', opacity: modalIsOpen? '0.55' :''}} className="home">
+    <div
+      style={{
+        backgroundColor: modalIsOpen ? "black" : "",
+        opacity: modalIsOpen ? "0.55" : "",
+      }}
+      className="home"
+    >
       <div className="homeLeft">
         <div id="Flama" class="Flama">
           <div className="flama_m" id="Flama_m">
@@ -53,30 +50,23 @@ function Home() {
       </div>
 
       <div className="homeRight">
-        <div className="bio" onClick={()=>setModalIsOpen(true)}>bio</div>
-        <div className="bio"  >Gallery</div>
+        <div className="bio" onClick={() => setModalIsOpen(true)}>
+          bio
+        </div>
+        <div className="bio">Gallery</div>
         <div className="bio">musica</div>
       </div>
-    
-       
-       <Modal
-         isOpen={modalIsOpen}
+
+      <Modal
+        isOpen={modalIsOpen}
         //  onAfterOpen={afterOpenModal}
-         onRequestClose={()=>setModalIsOpen(false)}
-         style={customStyles}
-         contentLabel="Example Modal"
-         id='Modal'
-         
-       >
-         {/* <Gallery/> */}
-        
-          
-         <button onClick={()=>setModalIsOpen(false)}>close</button>
-         <div>I am a modal</div>
-       
-         
-       </Modal>
-     
+        onRequestClose={() => setModalIsOpen(false)}
+        style={customStyles}
+        contentLabel="Example Modal"
+        id="Modal"
+      >
+        <Gallery setModalIsOpen={setModalIsOpen} />
+      </Modal>
     </div>
   );
 }
